251790db146bb691f4a4de572cce82a7222be4ae
Suraj-nv <dharasuraj8@gmail.com>
2026-08-10 14:20:06 +0530

    Update Django project

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
-     return render(request, "accounts/index.html")
+    return HttpResponse("Hello, World! This is the home page.")
+# def home(request):
+#      return render(request, "accounts/index.html")
 
 # Create your views here.
